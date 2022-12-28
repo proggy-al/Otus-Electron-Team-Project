473648af4720c293028c0a7e25d4400623295d35
Mikhail Dmitriev <mikdmitr@gmail.com>
2022-12-29 00:02:25 +0300

    core for Identity #11

---
+++ src/GMS.Identity/GMS.Identity.Core/Abstractions/Repositories/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using GMS.Identity.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GMS.Identity.Core.Abstractions.Repositories
+{
+    public interface IRepository<T> where T : BaseEntity
+
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+
+        Task<T> GetByIdAsync(Guid id);
+
+
+    }
+}
---
+++ src/GMS.Identity/GMS.Identity.DataAccess/Repositories/UserRepository.cs
@@ -0,0 +1,23 @@
+﻿using GMS.Identity.Core.Abstractions.Repositories;
+using GMS.Identity.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GMS.Identity.DataAccess.Repositories
+{
+    public class UserRepository<T> : IRepository<T> where T : BaseEntity
+    {
+        public Task<IEnumerable<T>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
