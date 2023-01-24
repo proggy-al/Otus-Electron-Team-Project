314314a5c4d8f7966b5508d506f2a6b144581b39
otususer <inmirrett@gmail.com>
2023-01-24 11:32:47 +0300

    Добавлена заготовка базы данных

---
+++ src/GMS.Communication/GMS.Communication.DataAccess/Context/MesseageDb.cs
@@ -0,0 +1,15 @@
+﻿using GMS.Communication.Core.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GMS.Communication.DataAccess.Context
+{
+    public class MesseageDb
+    {
+        DbSet<GmsMessage>  GmsMesseage {get;set;}
+    }
+}
