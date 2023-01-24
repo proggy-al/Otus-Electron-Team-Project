fb36d1244014046e22dfecffcfdcac9295266a09
otususer <inmirrett@gmail.com>
2023-01-24 11:29:33 +0300

    Добавлена доменная модель

---
+++ src/GMS.Communication/GMS.Communication.Core/Enums.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GMS.Communication.Core;
+
+public enum MessageType
+{
+    Unknown = 0,
+}
