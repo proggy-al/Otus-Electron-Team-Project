7de777005866d91f5729284d974d81a30195e2c7
Федор Шарапов <sharapovfedor@yandex.ru>
2023-02-13 12:31:28 +0100

    исходная структура проекта

---
+++ src/GMS.Core/GMS.Core.WebHost/Controllers/AreaController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GMS.Core.WebHost.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AreaController : ControllerBase
+    {
+
+    }
+}
---
+++ src/GMS.Core/GMS.Core.WebHost/Program.cs
@@ -0,0 +1,34 @@
+using GMS.Core.DataAccess.Context;
+using GMS.Core.WebHost.Configurations;
+using Serilog;
+
+try
+{
+    var builder = WebApplication.CreateBuilder(args);
+    builder.Environment.ApplicationName = typeof(Program).Assembly.FullName;
+
+    builder.Services.ConfigureLogger();
+    builder.Services.ConfigureMapper();
+    builder.Services.AddDbContext<DatabaseContext>();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.ConfigureSwagger();
+    builder.Services.AddAuthorization();
+    builder.Services.AddControllers();
+
+    var app = WebApplicationConfiguration.Configure(builder);
+
+    Log.Logger.Information($"The {app.Environment.ApplicationName} started...");
+
+    app.Run();
+
+    return 0;
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Host terminated unexpectedly!");
+    return 1;
+}
+finally
+{
+    Log.CloseAndFlush();
+}
