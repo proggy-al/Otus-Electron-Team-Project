b106d1e211fbc970231a1a25284d86d37a309d06
Федор Шарапов <sharapovfedor@yandex.ru>
2023-02-13 12:15:42 +0100

    Добавлен интерфейс сущности с идентификатором

---
+++ src/GMS.Core/GMS.Core.Core/Abstractions/Repositories/Base/IEntity.cs
@@ -0,0 +1,14 @@
+﻿namespace GMS.Core.Core.Abstractions.Repositories.Base
+{
+    /// <summary>
+    /// Интерфейс сущности с идентификатором
+    /// </summary>
+    /// <typeparam name="TId">Тип идентификатора</typeparam>
+    public interface IEntity<TId>
+    {
+        /// <summary>
+        /// Идентификатор
+        /// </summary>
+        TId Id { get; set; }
+    }
+}
