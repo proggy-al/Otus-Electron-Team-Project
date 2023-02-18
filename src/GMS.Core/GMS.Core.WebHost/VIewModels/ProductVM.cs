fa94e26f6957eaa6307c4865cf18a8498e214104
Федор Шарапов <sharapovfedor@yandex.ru>
2023-02-19 00:28:59 +0100

    добавлены ViewModels для всех сущностей

---
+++ src/GMS.Core/GMS.Core.WebHost/VIewModels/ProductVM.cs
@@ -0,0 +1,43 @@
+﻿namespace GMS.Core.WebHost.VIewModels
+{
+    /// <summary>
+    /// ДТО продукта
+    /// </summary>
+    public class ProductVM
+    {
+        /// <summary>
+        /// Идентификатор
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// Наименование
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Описание
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Количество (тренировок в пакете персональных тренировок, дней в абонементе на свободное посещение, минут в абонементе на солярий)
+        /// </summary>
+        public int Quantity { get; set; }
+
+        /// <summary>
+        /// Цена
+        /// </summary>
+        public int Price { get; set; }
+
+        /// <summary>
+        /// Идентификатор фитнес клуба
+        /// </summary>
+        public Guid FitnessClubId { get; set; }
+
+        /// <summary>
+        /// Удалено
+        /// </summary>
+        public bool Deleted { get; set; }
+    }
+}
