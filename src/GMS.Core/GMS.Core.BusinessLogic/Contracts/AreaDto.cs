8d8750ccf0cc55ca2b2dc316dd9a5d22135fc769
Федор Шарапов <sharapovfedor@yandex.ru>
2023-02-19 00:25:16 +0100

    добавлены Dto для всех сущностей

---
+++ src/GMS.Core/GMS.Core.BusinessLogic/Contracts/AreaDto.cs
@@ -0,0 +1,28 @@
+﻿namespace GMS.Core.BusinessLogic.Contracts
+{
+    /// <summary>
+    /// ДТО зоны
+    /// </summary>
+    public class AreaDto
+    {   
+        /// <summary>
+        /// Идентификатор
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// Наименование
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Идентификатор фитнес клуба
+        /// </summary>
+        public Guid FitnessClubId { get; set; }
+
+        /// <summary>
+        /// Удалено
+        /// </summary>
+        public bool Deleted { get; set; }
+    }
+}
---
+++ src/GMS.Core/GMS.Core.BusinessLogic/Contracts/ContractDto.cs
@@ -0,0 +1,48 @@
+﻿namespace GMS.Core.BusinessLogic.Contracts
+{
+    /// <summary>
+    /// ДТО контракта
+    /// </summary>
+    public class ContractDto
+    {
+        /// <summary>
+        /// Идентификатор
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// Идентификатор продукта
+        /// </summary>
+        public Guid ProductId { get; set; }
+
+        /// <summary>
+        /// Идентификатор менеджера
+        /// </summary>
+        public Guid ManagerId { get; set; }
+
+        /// <summary>
+        /// Идентификатор пользователя
+        /// </summary>
+        public Guid UserId { get; set; }
+
+        /// <summary>
+        /// Идентификатор фитнес клуба
+        /// </summary>
+        public Guid FitnessClubId { get; set; }
+
+        /// <summary>
+        /// Дата начала контракта
+        /// </summary>
+        public DateTime StartDate { get; set; }
+
+        /// <summary>
+        /// Дата завершения контракта
+        /// </summary>
+        public DateTime EndDate { get; set; }
+
+        /// <summary>
+        /// Удалено
+        /// </summary>
+        public bool Deleted { get; set; }
+    }
+}
