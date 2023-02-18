a79775c053928887ad40826f06a3950516d6173d
Федор Шарапов <sharapovfedor@yandex.ru>
2023-02-19 00:27:50 +0100

    регистрация профелей автомапера в проекте WebHost

--- src/GMS.Core/GMS.Core.WebHost/Configurations/AutoMapperConfiguration.cs
+++ src/GMS.Core/GMS.Core.WebHost/Configurations/AutoMapperConfiguration.cs
@@ -1,18 +1,34 @@
 ﻿using AutoMapper;
+using GMS.Core.BusinessLogic.Mappings;
+using GMS.Core.WebHost.Mappings;
 
 namespace GMS.Core.WebHost.Configurations
 {
     public static class AutoMapperConfiguration
     {
-        public static void ConfigureMapper(this IServiceCollection services)
+        public static IServiceCollection ConfigureMapper(this IServiceCollection serviceCollection)
         {
-            services.AddSingleton<IMapper>(new Mapper(Configuration()));
+            serviceCollection.AddSingleton<IMapper>(new Mapper(Configuration()));
+            return serviceCollection;
         }
+
         private static MapperConfiguration Configuration()
         {
             var configuration = new MapperConfiguration(cfg =>
             {
-                //cfg.AddProfile<EntityNameCreateMappingsProfile>();
+                cfg.AddProfile<FitnessClubMappingsProfile>();
+                cfg.AddProfile<AreaMappingsProfile>();
+                cfg.AddProfile<ProductMappingsProfile>();
+                cfg.AddProfile<ContractMappingsProfile>();
+                cfg.AddProfile<TimeSlotMappingsProfile>();
+                cfg.AddProfile<TrainingMappingsProfile>();
+
+                cfg.AddProfile<FitnessClubVmMappingsProfile>();
+                cfg.AddProfile<AreaVmMappingsProfile>();
+                cfg.AddProfile<ProductVmMappingsProfile>();
+                cfg.AddProfile<ContractVmMappingsProfile>();
+                cfg.AddProfile<TimeSlotVmMappingsProfile>();
+                cfg.AddProfile<TrainingVmMappingsProfile>();
             });
 
             configuration.AssertConfigurationIsValid();
