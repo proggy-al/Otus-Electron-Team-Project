8e03922265f28bb2492b13eab8d99094a023a585
Федор Шарапов <sharapovfedor@yandex.ru>
2023-02-19 00:29:25 +0100

    добавлены профили автомаперов для всех сущностей

---
+++ src/GMS.Core/GMS.Core.WebHost/Mappings/AreaVmMappingsProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using GMS.Core.BusinessLogic.Contracts;
+using GMS.Core.WebHost.VIewModels;
+
+namespace GMS.Core.WebHost.Mappings
+{
+    /// <summary>
+    /// Профиль автомаппера для сущности зоны
+    /// </summary>
+    public class AreaVmMappingsProfile : Profile
+    {
+        public AreaVmMappingsProfile()
+        {
+            CreateMap<AreaVM, AreaDto>();
+            CreateMap<AreaDto, AreaVM>();
+        }
+    }
+}
