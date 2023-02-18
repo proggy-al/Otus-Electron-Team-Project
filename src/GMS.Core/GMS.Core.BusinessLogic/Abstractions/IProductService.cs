40bb2a9facab4f34be13999fdb7c934b49059793
Федор Шарапов <sharapovfedor@yandex.ru>
2023-02-19 00:26:19 +0100

    добавлены интерфейсы сервисов бизнес логики

---
+++ src/GMS.Core/GMS.Core.BusinessLogic/Abstractions/IProductService.cs
@@ -0,0 +1,34 @@
+﻿using GMS.Core.BusinessLogic.Abstractions.Base;
+using GMS.Core.BusinessLogic.Contracts;
+using GMS.Core.Core.Domain;
+using GMS.Core.DataAccess.Repositories;
+
+namespace GMS.Core.BusinessLogic.Abstractions
+{
+    public interface IProductService : IBaseService<ProductRepository, Product, ProductDto, Guid>
+    {
+        /// <summary>
+        /// Получить список
+        /// </summary>
+        /// <param name="page">номер страницы</param>
+        /// <param name="itemsPerPage">объем страницы</param>
+        /// <returns>список ДТО продукта</returns>
+        Task<List<ProductDto>> GetPage(int page, int itemsPerPage);
+
+        /// <summary>
+        /// Получить все сущности по идентификатору фитнес клуба 
+        /// </summary>
+        /// <param name="fitnessClubId">идентификатор фитнес клуба</param>
+        /// <returns>список ДТО продуктов</returns>
+        Task<List<ProductDto>> GetAllByFitnessClubId(Guid fitnessClubId);
+
+        /// <summary>
+        /// Получить постраничный список по идентификатору фитнес клуба 
+        /// </summary>
+        /// <param name="fitnessClubId">идентификатор фитнес клуба</param>
+        /// <param name="page">номер страницы</param>
+        /// <param name="itemsPerPage">объем страницы</param>
+        /// <returns>список ДТО продуктов</returns>
+        Task<List<ProductDto>> GetPageByFitnessClubId(Guid fitnessClubId, int page, int itemsPerPage);
+    }
+}
---
+++ src/GMS.Core/GMS.Core.BusinessLogic/Abstractions/ITimeSlotService.cs
@@ -0,0 +1,19 @@
+﻿using GMS.Core.BusinessLogic.Abstractions.Base;
+using GMS.Core.BusinessLogic.Contracts;
+using GMS.Core.Core.Domain;
+using GMS.Core.DataAccess.Repositories;
+
+namespace GMS.Core.BusinessLogic.Abstractions
+{
+    public interface ITimeSlotService : IBaseService<TimeSlotRepository, TimeSlot, TimeSlotDto, Guid>
+    {
+        /// <summary>
+        /// Получить список временных интервалов за день 
+        /// </summary>
+        /// <param name="date">дата</param>
+        /// <param name="fitnessClubId">идентификатор фитнес клуба</param>
+        /// <param name="trainerId">идентификатор тренера</param>
+        /// <returns>список ДТО временных интервалов</returns>
+        Task<List<TimeSlotDto>> GetAllPerDay(DateOnly date, Guid fitnessClubId, Guid trainerId);
+    }
+}
