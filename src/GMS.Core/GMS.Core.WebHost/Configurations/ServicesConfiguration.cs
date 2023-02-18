2b17249b40a3a9718a49940b5193b2e4a84ef6e7
Федор Шарапов <sharapovfedor@yandex.ru>
2023-02-19 00:28:21 +0100

    регистрация сервисов в DI

---
+++ src/GMS.Core/GMS.Core.WebHost/Configurations/ServicesConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using GMS.Core.BusinessLogic.Abstractions;
+using GMS.Core.BusinessLogic.Services;
+
+namespace GMS.Core.WebHost.Configurations
+{
+    public static class ServicesConfiguration
+    {
+        public static IServiceCollection AddServices(this IServiceCollection serviceCollection)
+        {
+            serviceCollection
+                .AddTransient<IFitnessClubService, FitnessClubService>()
+                .AddTransient<IAreaService, AreaService>()
+                .AddTransient<IProductService, ProductService>()
+                .AddTransient<IContractService, ContractService>()
+                .AddTransient<ITimeSlotService, TimeSlotService>()
+                .AddTransient<ITrainingService, TrainingService>();
+
+            return serviceCollection;
+        }
+    }
+}
