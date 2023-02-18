ca7f1fe00b24a49d08148792ce0ac6521986d940
Федор Шарапов <sharapovfedor@yandex.ru>
2023-02-19 00:25:47 +0100

    добавлены профили автомаппера для всех сущностей

---
+++ src/GMS.Core/GMS.Core.BusinessLogic/Mappings/TimeSlotMappingsProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using GMS.Core.BusinessLogic.Contracts;
+using GMS.Core.Core.Domain;
+
+namespace GMS.Core.BusinessLogic.Mappings
+{
+    /// <summary>
+    /// Профиль автомаппера для сущности временного интервала
+    /// </summary>
+    public class TimeSlotMappingsProfile : Profile
+    {
+        public TimeSlotMappingsProfile()
+        {
+            CreateMap<TimeSlot, TimeSlotDto>();
+
+            CreateMap<TimeSlotDto, TimeSlot>()
+                .ForMember(d => d.FitnessClub, map => map.Ignore())
+                .ForMember(d => d.Area, map => map.Ignore())
+                .ForMember(d => d.Training, map => map.Ignore());
+        }
+    }
+}
