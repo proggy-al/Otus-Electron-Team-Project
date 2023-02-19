d8f3aefd864dffb9c2572a2eef8f90650838ae71
Serjo85 <bobrosebob07@gmail.com>
2023-02-20 01:37:37 +0300

    Добавлены классы контролера и хаба

---
+++ src/GMS.Communication/GMS.Communication.WebHost/Controllers/MessagesController.cs
@@ -0,0 +1,24 @@
+﻿using GMS.Communication.WebHost.Hubs;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+
+namespace GMS.Communication.WebHost.Controllers
+{
+    public class MessagesController : Controller
+    {
+        private readonly IHubContext<ChatHub> _hubContext;
+
+        public MessagesController(IHubContext<ChatHub> hubContext)
+        {
+                _hubContext = hubContext;
+        }
+
+        [Route("Send")]
+        [HttpPost]
+        public IActionResult SendMessage(Guid userId, string message) //TODO Добавить DTO сообщения
+        {
+            _hubContext.Clients.All.SendAsync("ReceiveMessage", userId, message);
+            return Ok();
+        }
+    }
+}
---
+++ src/GMS.Communication/GMS.Communication.WebHost/Hubs/ChatHub.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+
+namespace GMS.Communication.WebHost.Hubs
+{
+    public class ChatHub : Hub
+    {
+        //[Authorize]
+        public async Task SendMessage(string message, Guid userId)
+        {
+            //await Clients.User(userId).SendAsync("Receive", message, userName);
+            await Clients.All.SendAsync("ReceiveMessage", userId, message);
+        }
+    }
+}
