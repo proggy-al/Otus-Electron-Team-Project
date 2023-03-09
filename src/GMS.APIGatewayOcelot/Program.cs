81271d83996bf266d78f686764ba7bb620efff8d
Mikhail Dmitriev <mikdmitr@gmail.com>
2023-03-09 23:12:40 +0300

    some improvements for dockerfile in  core service and Ocelot api gateway service #54

--- src/GMS.APIGatewayOcelot/Program.cs
+++ src/GMS.APIGatewayOcelot/Program.cs
@@ -1,14 +1,21 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 namespace GMS.APIGatewayOcelot
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
+                .AddJsonFile("ocelot.json",optional:false, reloadOnChange: true)
+                .AddEnvironmentVariables();
+            builder.Services.AddOcelot(builder.Configuration);
+                     
             var app = builder.Build();
-
-            app.MapGet("/", () => "Hello World!");
-
+            await app.UseOcelot();
+                        
             app.Run();
         }
     }
