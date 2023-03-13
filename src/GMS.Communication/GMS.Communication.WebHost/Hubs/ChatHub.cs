59e5c400b495291cde952d4bd7b3e528ef8e3760
Serjo85 <bobrosebob07@gmail.com>
2023-03-13 23:07:33 +0300

    Исправлен нейминг

--- src/GMS.Communication/GMS.Communication.WebHost/Hubs/ChatHub.cs
+++ src/GMS.Communication/GMS.Communication.WebHost/Hubs/ChatHub.cs
@@ -3,13 +3,23 @@ using Microsoft.AspNetCore.SignalR;
 
 namespace GMS.Communication.WebHost.Hubs
 {
+
     public class ChatHub : Hub
     {
-        //[Authorize]
-        public async Task SendMessage(string message, Guid userId)
+        [Authorize]
+        public async Task SendMessageAsync(string message, Guid userId)
         {
             //await Clients.User(userId).SendAsync("Receive", message, userId);
             await Clients.All.SendAsync("ReceiveMessage", message);
+
+        }
+
+        [Authorize]
+        public override async Task OnConnectedAsync()
+        {            
+            //var userName = Context.User?.Identity?.Name ?? "Anonymous";
+            //var connectionId = Context.ConnectionId;
+            await base.OnConnectedAsync();
         }
     }
 }
