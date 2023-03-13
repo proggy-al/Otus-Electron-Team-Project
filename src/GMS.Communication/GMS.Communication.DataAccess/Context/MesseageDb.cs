4aa179e2db151100672b0268479ac1b7442dc01f
Serjo85 <bobrosebob07@gmail.com>
2023-03-13 23:06:45 +0300

    Переработано сообщение для чата

--- src/GMS.Communication/GMS.Communication.DataAccess/Context/MesseageDb.cs
+++ src/GMS.Communication/GMS.Communication.DataAccess/Context/MesseageDb.cs
@@ -1,15 +1,16 @@
 ﻿using GMS.Communication.Core.Domain;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Security.Cryptography.X509Certificates;
 
 namespace GMS.Communication.DataAccess.Context
 {
     public class MesseageDb
     {
-        DbSet<GmsMessage>  GmsMesseage {get;set;}
+        DbSet<ChatMessage>  GmsMesseage {get;set;}
+
+        public MesseageDb()
+        {
+            
+        }
     }
 }
