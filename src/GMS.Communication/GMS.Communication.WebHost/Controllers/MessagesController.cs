e75e72707b3f7fafa69dfd13fb712cfbdbc78dfc
Mikhail Dmitriev <mikdmitr@gmail.com>
2023-03-15 00:55:44 +0300

    берем ID пользователя из контекста #38

--- src/GMS.Communication/GMS.Communication.WebHost/Controllers/MessagesController.cs
+++ src/GMS.Communication/GMS.Communication.WebHost/Controllers/MessagesController.cs
@@ -2,6 +2,9 @@
 using GMS.Communication.WebHost.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SignalR;
+using JWTAuthManager;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
 
 namespace GMS.Communication.WebHost.Controllers
 {
@@ -16,18 +19,27 @@ namespace GMS.Communication.WebHost.Controllers
             _hubContext = hubContext;
         }
 
+        [RequirePrivelege(Priviliges.Administrator, Priviliges.System)] //ограничения доступа к ручке
         [Route("SendMessageToAll")]
         [HttpPost]
         public async Task<IActionResult> SendMessageToAll([FromBody]MessageRequestDTO request)
         {
+            //так мы получаем ID авторизованного юзера
+            var id= User.Claims.FirstOrDefault(a => a.Type == "ID").Value;
+
+
             await _hubContext.Clients.All.SendAsync("ReceiveMessage", request.Subject, request.Body);
             return Ok();
         }
 
+        [Authorize] //ограничения доступа к ручке
         [Route("SendMessageById")]
         [HttpPost]
         public async Task<IActionResult> SendMessageById([FromBody]MessageRequestDTO request)
-        {            
+        {
+            //так мы получаем ID авторизованного юзера
+            var id = User.Claims.FirstOrDefault(a => a.Type == "ID").Value;
+
             await _hubContext.Clients.User(request.RecipientId.ToString()).SendAsync("ReceiveMessage", request.Subject, request.Body);
             return Ok();
         }
