3e887185ab45d97849fbac5d3a9a747a127249dd
Serjo85 <bobrosebob07@gmail.com>
2023-03-25 18:57:23 +0300

    Тестовые данные для инициализации БД

---
+++ src/GMS.Communication/GMS.Communication.DataAccess/Data/TestData.cs
@@ -0,0 +1,39 @@
+﻿using GMS.Communication.Core;
+using GMS.Communication.Core.Domain;
+
+namespace GMS.Communication.DataAccess.Data
+{
+    public static class TestData
+    {
+        public static List<GmsMessage> Messages = new List<GmsMessage>()
+        {
+            new GmsMessage()
+            {
+                RecipientId = new Guid(),
+                SenderId = new Guid(),
+                Subject = "TestSubject1",
+                Body = "TestBody1",
+                Type = MessageType.Text,
+                Status = MessageStatus.read,
+            },
+            new GmsMessage()
+            {
+                RecipientId = new Guid(),
+                SenderId = new Guid(),
+                Subject = "TestSubject2",
+                Body = "TestBody2",
+                Type = MessageType.Text,
+                Status = MessageStatus.undelivered,
+            },
+            new GmsMessage()
+            {
+                RecipientId = new Guid(),
+                SenderId = new Guid(),
+                Subject = "TestSubject3",
+                Body = "TestBody3",
+                Type = MessageType.Text,
+                Status = MessageStatus.unread,
+            }
+        };
+    }
+}
