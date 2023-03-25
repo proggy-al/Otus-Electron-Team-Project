54ba860cfbbcb77904ee77dd91c32f8429c7f5f6
Serjo85 <bobrosebob07@gmail.com>
2023-03-25 17:11:55 +0300

    Инициализация WebHost вынесена в Startup

--- src/GMS.Communication/GMS.Communication.WebHost/Program.cs
+++ src/GMS.Communication/GMS.Communication.WebHost/Program.cs
@@ -3,33 +3,25 @@ using GMS.Communication.WebHost.Models;
 using JWTAuthManager;
 using Microsoft.AspNetCore.SignalR;
 
-// TODO добавить реалзицию Stratup для разделения наполнения IoC и Pipeline
-var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddSingleton<IUserIdProvider, MyUserProvider>();
-builder.Services.AddSignalR();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddControllers();
-builder.Services.AddCustomJWTAuthentification();    
-
-
-var app = builder.Build();
-
-//if (app.Environment.IsDevelopment())
-//{
-    app.UseSwagger();
-    app.UseSwaggerUI(options =>
+namespace GMS.Communication.WebHost
+{
+    public class Program
     {
-        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
-        options.RoutePrefix = string.Empty;
-    });
-//}
+        public static async Task Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+            await host.RunAsync();
+        }
 
-//app.UseHttpsRedirection();
-app.UseRouting();
+        public static IHostBuilder CreateHostBuilder(string[] args) => Host.CreateDefaultBuilder(args)
+            .ConfigureLogging((context, logging) =>
+            {
+                logging.AddConsole();
+            })
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            });
+    }
+}
 
-app.MapControllers();
-app.UseAuthentication();
-app.UseAuthorization();
-app.MapHub<ChatHub>($"/chatHub");
-app.Run();
---
+++ src/GMS.Communication/GMS.Communication.WebHost/Startup.cs
@@ -0,0 +1,57 @@
+﻿using GMS.Communication.DataAccess.Context;
+using GMS.Communication.WebHost.Hubs;
+using GMS.Communication.WebHost.Models;
+using JWTAuthManager;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
+
+namespace GMS.Communication.WebHost
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<GmsMessagesDb>(options =>
+            {
+                options.UseSqlite(
+                    "Data Source=GmsMessages.db",
+                    x => x.MigrationsAssembly("GMS.Communication.DataAccess.Sqlite")
+                    );
+            });
+            services.AddSingleton<IUserIdProvider, MyUserProvider>();
+            services.AddSignalR();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+            services.AddControllers();
+            services.AddCustomJWTAuthentification();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+                options.RoutePrefix = string.Empty;
+            });
+
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapHub<ChatHub>($"/chatHub");
+            });
+            app.UseAuthentication();
+            app.UseAuthorization();
+        }
+    }
+}
