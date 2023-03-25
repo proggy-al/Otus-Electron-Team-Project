a55cb52ce2238dbbe94bdc8c60558f3fcb3da7a4
Serjo85 <bobrosebob07@gmail.com>
2023-03-25 19:09:17 +0300

    Добавлен инициализация БД.

--- src/GMS.Communication/GMS.Communication.WebHost/Program.cs
+++ src/GMS.Communication/GMS.Communication.WebHost/Program.cs
@@ -1,18 +1,25 @@
-using GMS.Communication.WebHost.Hubs;
-using GMS.Communication.WebHost.Models;
-using JWTAuthManager;
-using Microsoft.AspNetCore.SignalR;
+using GMS.Communication.DataAccess.Context;
+using GMS.Communication.DataAccess.Data;
 
 namespace GMS.Communication.WebHost
 {
     public class Program
     {
+        /// <summary>
+        /// Entry point
+        /// </summary>
+        /// <param name="args"></param>
         public static async Task Main(string[] args)
         {
             var host = CreateHostBuilder(args).Build();
             await host.RunAsync();
         }
 
+        /// <summary>
+        /// CreateHostBuilder
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
         public static IHostBuilder CreateHostBuilder(string[] args) => Host.CreateDefaultBuilder(args)
             .ConfigureLogging((context, logging) =>
             {
@@ -22,6 +29,20 @@ namespace GMS.Communication.WebHost
             {
                 webBuilder.UseStartup<Startup>();
             });
+
+        /// <summary>
+        /// Creates a database if it doesn't exist
+        /// </summary>
+        /// <param name="host"></param>
+        /// <param name="logger"></param>
+        /// <returns></returns>
+        private static async Task CreateDbIfNotExist(IHost host, ILogger<GmsMessagesDb> logger)
+        {
+            await using var scope = host.Services.CreateAsyncScope();
+            var services = scope.ServiceProvider;
+            var context = services.GetRequiredService<GmsMessagesDb>();
+            await DbInitializer.InitializeAsync(context, logger, true);
+        }
     }
 }
 
