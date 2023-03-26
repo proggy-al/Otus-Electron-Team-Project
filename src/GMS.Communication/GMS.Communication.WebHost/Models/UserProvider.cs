9d6890ef46b1c64b2952920a1a7cc9ee0fa57b2e
Serjo85 <bobrosebob07@gmail.com>
2023-03-26 22:52:30 +0300

    UserProvider переписан с тестового на боевую реализацию

--- src/GMS.Communication/GMS.Communication.WebHost/Models/UserProvider.cs
+++ src/GMS.Communication/GMS.Communication.WebHost/Models/UserProvider.cs
@@ -6,8 +6,8 @@ namespace GMS.Communication.WebHost.Models
     {
         public string? GetUserId(HubConnectionContext connection)
         {
-            var x = connection.User.Claims.FirstOrDefault(x => x.Type == "ID");
-            return "c35e71b1-01fe-4e96-aa13-35371f792a4f";
+            var id = connection.User.Claims.FirstOrDefault(x => x.Type == "ID");
+            return id?.ToString();
         }
     }
 }
