1b065c924bab690c510f13bcdc5469a9a55a9881
Федор Шарапов <sharapovfedor@yandex.ru>
2023-03-31 11:45:35 +0200

    #86 добавлена аутентификация

--- src/GMS.Core/GMS.Core.WebHost/Configurations/SwaggerConfiguration.cs
+++ src/GMS.Core/GMS.Core.WebHost/Configurations/SwaggerConfiguration.cs
@@ -6,14 +6,39 @@ namespace GMS.Core.WebHost.Configurations
     {
         public static IServiceCollection ConfigureSwagger(this IServiceCollection serviceCollection)
         {
-
+            
             serviceCollection.AddSwaggerGen(options =>
             {
+                
                 options.SwaggerDoc("v1", new OpenApiInfo
                 {
                     Description = "GMS API v1",
-                    Title = "Swagger", 
+                    Title = "Swagger",
                     Version = "1.0.0"
+                });
+
+                options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+                {
+                    Name = "JWT Bearer authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "JWT Authorization header using the Bearer scheme.\r\nYou should get the token in GMS.Identity.WebHost and enter your token in the text input below.",
+                });
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[] {}
+                    }
                 });
             });
             return serviceCollection;
