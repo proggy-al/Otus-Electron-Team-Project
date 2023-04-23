2d7cedf4707381b86e5c44b2acc9f9b45d2dc65a
Serjo85 <bobrosebob07@gmail.com>
2023-04-23 21:32:26 +0300

    i123 Созданы DTO для создания уведомления

---
+++ src/GMS.Communication/GMS.Communication.WebHost/Models/TrainingNotificationCreateDTO.cs
@@ -0,0 +1,19 @@
+﻿namespace GMS.Communication.WebHost.Models
+{
+    public class TrainingNotificationCreateDTO
+    {
+        public Guid TrainingId { get; set; }
+        public Guid UserId { get; set; }
+        public string Email { get; set; }
+        public string userName { get; set; }
+        public string DateTime { get; set; }
+        public string TrainingTitle { get; set; }
+    }
+
+    public class TrainingNotification
+    {
+        public Guid? UserId { get; set; }
+        public string? Email { get; set; }
+        public string Content { get; set; }
+    }
+}
