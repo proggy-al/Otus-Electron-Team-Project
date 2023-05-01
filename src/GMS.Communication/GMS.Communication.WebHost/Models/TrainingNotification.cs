c6e8b84d5b31e3867d6bceb5b5dba79cb5150456
Serjo85 <bobrosebob07@gmail.com>
2023-05-01 19:40:28 +0300

    i123 TraningNotification вынесена в отдельный класс и перемещена в Core/Domain

---
+++ src/GMS.Communication/GMS.Communication.WebHost/Models/TrainingNotification.cs
@@ -0,0 +1,24 @@
+﻿using GMS.Communication.Core.Abstractons;
+
+namespace GMS.Communication.WebHost.Models
+{
+    /// <summary>
+    /// Уведомление о тренировке
+    /// </summary>
+    /// <remarks>
+    /// Id уведомления совпадает с Id тренировки т.к. одна тренировка один клиент
+    /// </remarks>
+    public class TrainingNotification: BaseEntity
+    {
+        // Если заполнен будет сообщение в личный кабинет
+        public Guid? UserId { get; set; }
+        // Дата и время тренировки
+        public DateTime TrainingDateTime { get; set; }
+        // Время до тренировки когода произойдёт рассылка
+        public DateTime NotificationPeriod { get; set; }
+        // Если заполнен будет оповещение по Email
+        public string? Email { get; set; }
+        // Текст уведомленния
+        public string Content { get; set; }
+    }
+}
--- src/GMS.Communication/GMS.Communication.WebHost/Models/TrainingNotificationCreateDTO.cs
+++ src/GMS.Communication/GMS.Communication.WebHost/Models/TrainingNotificationCreateDTO.cs
@@ -1,6 +1,4 @@
-﻿using GMS.Communication.Core.Abstractons;
-
-namespace GMS.Communication.WebHost.Models
+﻿namespace GMS.Communication.WebHost.Models
 {
     /// <summary>
     /// Запрос на создание уведомления о тренировке
@@ -13,25 +11,5 @@ namespace GMS.Communication.WebHost.Models
         public string userName { get; set; }
         public string DateTime { get; set; }
         public string TrainingTitle { get; set; }
-    }
-
-    /// <summary>
-    /// Уведомление о тренировке
-    /// </summary>
-    /// <remarks>
-    /// Id уведомления совпадает с Id тренировки т.к. одна тренировка один клиент
-    /// </remarks>
-    public class TrainingNotification: BaseEntity
-    {
-        // Если заполнен будет сообщение в личный кабинет
-        public Guid? UserId { get; set; }
-        // Дата и время тренировки
-        public DateTime TrainingDateTime { get; set; }
-        // Время до тренировки когода произойдёт рассылка
-        public DateTime NotificationPeriod { get; set; }
-        // Если заполнен будет оповещение по Email
-        public string? Email { get; set; }
-        // Текст уведомленния
-        public string Content { get; set; }
     }
 }
