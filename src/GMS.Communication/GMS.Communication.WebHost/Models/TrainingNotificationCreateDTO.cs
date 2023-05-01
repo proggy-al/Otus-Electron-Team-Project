e3eded3db366eb3765d3a8cc53b4697ecb203510
Serjo85 <bobrosebob07@gmail.com>
2023-05-01 19:28:09 +0300

    i123 Иправлены классы моделей данных

--- src/GMS.Communication/GMS.Communication.WebHost/Models/TrainingNotificationCreateDTO.cs
+++ src/GMS.Communication/GMS.Communication.WebHost/Models/TrainingNotificationCreateDTO.cs
@@ -1,7 +1,12 @@
-﻿namespace GMS.Communication.WebHost.Models
+﻿using GMS.Communication.Core.Abstractons;
+
+namespace GMS.Communication.WebHost.Models
 {
+    /// <summary>
+    /// Запрос на создание уведомления о тренировке
+    /// </summary>
     public class TrainingNotificationCreateDTO
-    {
+    {        
         public Guid TrainingId { get; set; }
         public Guid UserId { get; set; }
         public string Email { get; set; }
@@ -10,10 +15,23 @@
         public string TrainingTitle { get; set; }
     }
 
-    public class TrainingNotification
+    /// <summary>
+    /// Уведомление о тренировке
+    /// </summary>
+    /// <remarks>
+    /// Id уведомления совпадает с Id тренировки т.к. одна тренировка один клиент
+    /// </remarks>
+    public class TrainingNotification: BaseEntity
     {
+        // Если заполнен будет сообщение в личный кабинет
         public Guid? UserId { get; set; }
+        // Дата и время тренировки
+        public DateTime TrainingDateTime { get; set; }
+        // Время до тренировки когода произойдёт рассылка
+        public DateTime NotificationPeriod { get; set; }
+        // Если заполнен будет оповещение по Email
         public string? Email { get; set; }
+        // Текст уведомленния
         public string Content { get; set; }
     }
 }
